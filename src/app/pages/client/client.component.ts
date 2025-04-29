af5cd8b70f8831e53e7569ea0517d289a9e01d79
karanroliyal <karanrawat.bca2021@imsuc.ac.in>
2025-04-29 23:53:43 +0530

    validation done state city api pending

--- src/app/pages/client/client.component.ts
+++ src/app/pages/client/client.component.ts
@@ -1,27 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms'
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms'
 import { PaginationComponent } from '../../components/pagination/pagination.component';
 import { ApiService } from '../../services/api.service';
 import { GlobalService } from '../../services/global.service';
+import { FormValidationMessageComponent } from '../../components/form-validation-message/form-validation-message.component';
 
 
-// interface table_data {
-//   id: string,
-//   name: string,
-//   email: string,
-//   phone: string,
-//   status: string,
-//   total_records: string,
-//   current_page: string,
-//   limit: string,
-//   total_pages: string
-// }
+
 
 
 @Component({
   selector: 'app-client',
-  imports: [ReactiveFormsModule, CommonModule, PaginationComponent],
+  imports: [ReactiveFormsModule, CommonModule, PaginationComponent,FormValidationMessageComponent],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
@@ -38,7 +29,7 @@ export class ClientComponent implements OnInit {
   }
 
   filterOption: boolean = false
-  table_data :  any
+  table_data: any
   total_records: number = 0
   page: number = 0
   limit: number = 0
@@ -55,6 +46,18 @@ export class ClientComponent implements OnInit {
     order: new FormControl("DESC"),
     sort_by: new FormControl('id'),
     page: new FormControl(1),
+  })
+
+  clientForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
+    email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(50)]),
+    phone: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+$/), Validators.maxLength(10), Validators.minLength(10)]),
+    state: new FormControl('', [Validators.required]),
+    district: new FormControl('', [Validators.required]),
+    status: new FormControl('', Validators.required),
+    profile: new FormControl('', Validators.required),
+    address: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required,Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).+$'), Validators.minLength(8), Validators.maxLength(15)]),
   })
 
 
@@ -77,18 +80,18 @@ export class ClientComponent implements OnInit {
     )
   }
 
-  resetFilterForm(){
+  resetFilterForm() {
     this.filterForm.get('page')?.setValue(1)
-    this.GF.preserveField(this.filterForm , ['action' , 'limit' , 'sort'] , null )
+    this.GF.preserveField(this.filterForm, ['action', 'limit', 'sort'], null)
     this.filterForm.patchValue({
-      status:'',
-      email:'',
-      limit:10,
-      order:'DESC',
-      sort_by:'id',
-      page:1,
-      name:'',
-      phone:''
+      status: '',
+      email: '',
+      limit: 10,
+      order: 'DESC',
+      sort_by: 'id',
+      page: 1,
+      name: '',
+      phone: ''
     })
     this.getTable()
   }
@@ -108,6 +111,28 @@ export class ClientComponent implements OnInit {
     this.filterForm.controls['order'].setValue(this.sortOrder);
     this.filterForm.controls['sort_by'].setValue(sortOn);
     this.getTable();
+  }
+
+  addClient() {
+    // Make password required
+    const passwordControl = this.clientForm.get('password');
+    passwordControl?.setValidators([Validators.required]);
+    passwordControl?.updateValueAndValidity();
+    this.clientForm.markAllAsTouched()
+    // if (this.clientForm.valid) {
+      this.api.postApi('add-pg-owner' , this.clientForm.value).subscribe(
+        (res:any)=>{
+          if(res.status){
+            this.GF.showToast(res.message , 'success')
+          }else{
+            this.GF.showToast(res.message , 'danger')
+          }
+        },
+        (err:any)=>{
+          this.GF.showToast(err.error.message , 'danger')
+        }
+      )
+    // }
   }
 
 }
